8c081e8a64a7b553eb60675f70757b75b20f4897
DasunNayanajith <110300288+DasunNayanajith@users.noreply.github.com>
2024-08-09 07:36:58 +0530

    Update config.php

--- test/config.php
+++ test/config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "tour_packages";
+$dbname = "stelaran_tour_packages";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
