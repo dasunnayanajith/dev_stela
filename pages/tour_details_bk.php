5708917cf4fa46bc109967798419e1e7b819e4d1
Dasun Nayanajith <linda.nayana96@gmail.com>
2024-11-23 10:46:02 +0530

    old restore tour details

---
+++ pages/tour_details_bk.php
@@ -0,0 +1,53 @@
+<!--==============================
+    Breadcumb
+============================== -->
+    <div class="breadcumb-wrapper " data-bg-src="assets/img/bg/breadcumb-bg.jpg">
+        <div class="container">
+            <div class="breadcumb-content">
+                <h1 class="breadcumb-title">Tour Details</h1>
+                <ul class="breadcumb-menu">
+                    <li><a href="index.php?page=home-travel">Home</a></li>
+                    <li>Tour Details</li>
+                </ul>
+            </div>
+        </div>
+    </div><!--==============================
+tour Area
+<!-- ==============================-->
+    <section class="space">
+
+     <!--====================================================TEST AREA=======================================================-->
+
+ <!--====================================================TEST AREA=======================================================-->
+           
+            <div class="location-map">
+                <h3 class="page-title mt-45 mb-30">Location</h3>
+                <div class="contact-map style3">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d1979.6639938990295!2d80.017571!3d7.087925!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2slk!4v1723311068981!5m2!1sen!2slk" allowfullscreen="" loading="lazy"></iframe>
+                    <div class="contact-icon">
+                        <img src="assets/img/icon/location-dot3.svg" alt="">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="shape-mockup about-shape movingX d-none d-xxl-block" data-bottom="40%" data-right="17%">
+            <img src="assets/img/normal/about-slide-img.png" alt="shape">
+        </div>
+        <div class="shape-mockup about-rating d-none d-xxl-block" data-bottom="48%" data-right="12%">
+            <i class="fa-sharp fa-solid fa-star"></i><span>4.9k</span>
+        </div>
+        <div class="shape-mockup about-emoji d-none d-xxl-block" data-bottom="45%" data-right="29%"><img src="assets/img/icon/emoji.png" alt="">
+        </div>
+        <div class="shape-mockup shape1 d-none d-xxl-block" data-bottom="55%" data-right="12%">
+            <img src="assets/img/shape/shape_1.png" alt="shape">
+        </div>
+        <div class="shape-mockup shape2 d-none d-xl-block" data-bottom="51%" data-right="8%">
+            <img src="assets/img/shape/shape_2.png" alt="shape">
+        </div>
+        <div class="shape-mockup shape3 d-none d-xxl-block" data-bottom="53%" data-right="5%">
+            <img src="assets/img/shape/shape_3.png" alt="shape">
+        </div>
+    </section><!--============================
+	Footer Area
+==============================-->
