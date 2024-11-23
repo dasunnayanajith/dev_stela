dd7cfe6dcbeb399836619340cd73e34ce91042cb
Dasun Nayanajith <linda.nayana96@gmail.com>
2024-11-23 20:34:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include('includes/config.php');
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 $page = preg_replace('/[^a-zA-Z0-9_]/', '', $page); // Sanitize input
 
-include 'header2.php';
+include 'header.php';
 
 $file = "pages/{$page}.php";
 if (file_exists($file)) {
